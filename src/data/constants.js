cf9f6a73f6ca4af8077c6d85ed99576e3a2f30d2
Gian-lucca <gianlucca.augusto@gmail.com>
2024-12-01 16:35:07 -0300

    Primeiros Ajustes

---
+++ src/data/constants.js
@@ -0,0 +1,282 @@
+export const Bio = {
+  name: "Gianlucca Augusto",
+  roles: [
+    "Web Developer",
+    "Analista PHP Drupal"
+  ],
+  description:
+    "Sou um indivíduo motivado e versátil, sempre ansioso para assumir novos desafios. Com paixão por aprender, dedico-me a entregar resultados de alta qualidade. Com uma atitude positiva e uma mentalidade de crescimento, estou pronto para fazer uma contribuição significativa e alcançar grandes coisas.",
+  github: "https://github.com/Gian-lucca",
+  resume:
+    "https://drive.google.com/drive/my-drive",
+  linkedin: "https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile",
+  insta: "https://www.instagram.com/gianlucaaugusto/",
+};
+
+export const skills = [
+  {
+    title: "",
+    skills: [
+      {
+        name: "Drupal",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/75/Druplicon.vector.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Typescript",
+        image:"https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg",
+      },
+      {
+        name: "Bootstrap",
+        image:"https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "PHP",
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:"https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Docker",
+        image:"https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Acquia Cloud",
+        image:"https://www.acquia.com/themes/custom/juice/logo.svg",
+      },
+      {
+        name: "Git",
+        image:"https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:"https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Figma",
+        image:"https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  }
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/D4D0BAQGlkyJOKvz6WQ/company-logo_200_200/company-logo_200_200/0/1718300941618/extremedigitalsolutions_logo?e=1741219200&v=beta&t=j0kmI2DX_VYE2SE7LL2SujJcPa63opG1ZN8dDR26cSI",
+    role: "Analista Desenvolvedor PHP Drupal",
+    company: "Extreme Digital Solutions - EDS",
+    date: "Julho 2021 - Momento",
+    desc: "Atualmente coordenador da equipe, focado exclusivamente no desenvolvimento de websites com o CMS Drupal, prestando serviços ao Proderj (Centro de Tecnologia da Informação e Comunicação do Estado do Rio de Janeiro).",
+    skills: [
+      "Drupal",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MySQL",
+      "PHP",
+      "Docker",
+      "Git",
+    ],
+    doc: "",
+    
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/v2/C510BAQGcRycrRp3kiA/company-logo_200_200/company-logo_200_200/0/1631368320150?e=1741219200&v=beta&t=Gw2WScqSvH5DKY5nBEkMElaxb2WHBlC8EHyhibANNLw",
+    role: "Desenvolvedor PHP Junior",
+    company: "Tecmaster Informática - Consultoria",
+    date: "Fevereiro 2024 - Agosto 2024",
+    desc: "Prestei serviços a Accenture na parte de Backend atuando no desenvolvimento de APIs utilizando o Drupal 10.",
+    skills: [
+      "Drupal",
+      "Acquia Cloud",
+      "Postman",
+      "Figma",
+    ],
+    doc: "",
+  },
+  {
+    id: 2,
+    img: "https://www.svgrepo.com/show/508699/landscape-placeholder.svg",
+    role: "JGLCONSERT",
+    company: "Sócio proprietário",
+    date: "Fevereiro 2021 - Junho 2021",
+    desc: "Loja de manutenção de aparelhos de celular e notebook.",
+  },
+  {
+    id: 3,
+    img: "https://media.licdn.com/dms/image/v2/C4D0BAQH7U5kE2bmoRw/company-logo_200_200/company-logo_200_200/0/1630530079634/icaiu_logo?e=1741219200&v=beta&t=PcATlgtcxc00HsqdzlnZPDoEbKen3GrUkyNa1_2vPn4",
+    role: "Técnico de celular Pleno",
+    company: "iCaiu",
+    date: "Novembro 2018 - Janeiro 2021",
+    desc: "Manutenção em iPhones e iPads, Atendimento ao cliente, Administrativo de toda loja, Responsável pelo treinamento de novos Técnicos, Responsável pela conferência e relatórios de estoque, Responsável pra solucionar todo e qualquer tipo de problemas que aparece-se em loja e Responsável por toda equipe",
+  },
+  {
+    id: 4,
+    img: "https://media.licdn.com/dms/image/v2/C4E0BAQEAWbb_xpJB6g/company-logo_200_200/company-logo_200_200/0/1630637071616/detro_departamento_de_transporte_rodovirio_do_rj_logo?e=1741219200&v=beta&t=stKc4D-sE6ZnIKFrjhO0xePKt6nX32CvrnZM9bz3ct0",
+    role: "Estagiário",
+    company: "DETRO - Departamento de Transporte Rodoviário do RJ",
+    date: "Março 2017 - Outubro 2018",
+    desc: " Responsável pelo atendimento ao público, Distribuição de processos e correspondência pelo órgão, Instrução de processos, Inclusão de processos no sistema ( UPO ) e Arquivista de processos.",
+  },
+  {
+    id: 5,
+    img: "https://gianlucca-augusto.vercel.app/images/images.jpg",
+    role: "Aprendiz",
+    company: "TIBRÁS - Iguaçu Utilidades e Papelaria Ltda",
+    date: "Agosto 2013 - Janeiro 2015",
+    desc: "Repositor, Estoquista de mercadorias e Atendimento ao cliente",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
+    school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
+    date: "Oct 2021 - Sep 2025",
+    grade: "8.41 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "Methodist School, Dankuni",
+    date: "Apr 2019 - Apr 2025",
+    grade: "88.2%",
+    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+    degree: "ISC(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "Methodist School, Dankuni",
+    date: "Apr 2017 - Apr 2019",
+    grade: "92.3%",
+    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
+    degree: "ICSC(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Portfólio",
+    description: "Meu Portfólio.",
+    image: "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
+    tags: [
+      "React Js",
+      "JavaScript",
+      "CSS",
+      "HTML",
+    ],
+    category: "machine learning",
+    webapp: "https://gianlucca-augusto.vercel.app/",
+    github: "https://github.com/Gian-lucca/Meu-site",
+  },
+  {
+    id: 9,
+    title: "Gerdau",
+    description: "Web site da Gerdau.",
+    image: "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "Drupal",
+      "PHP",
+      "Acquia Cloud",
+      "Postman",
+      "Figma",
+    ],
+    category: "web app",
+    webapp: "https://mais.gerdau.com.br/",
+  },
+  {
+    id: 0,
+    title: "PRODERJ",
+    description:"Web site do Centro de Tecnologia de Informação e Comunicação do Estado do Rio de Janeiro.",
+    image:"https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    tags: ["Drupal", "PHP", "MySQL", "HTML", "CSS", "JavaScript"],
+    category: "web app",
+    webapp: "https://www.rj.gov.br/proderj/",
+  },
+  {
+    id: 1,
+    title: "AGENERSA",
+    description: "Web site da Agência Reguladora de Energia e Saneamento Básico do Estado do Rio de Janeiro.",
+    image: "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: ["Drupal", "PHP", "MySQL", "HTML", "CSS", "JavaScript"],
+    category: "web app",
+    webapp: "https://www.rj.gov.br/agenersa/",
+  },
+  {
+    id: 2,
+    title: "Secretária de Saúde",
+    description:"Web site da Secretária de Saúde do Estado do Rio de Janeiro.",
+    image:"https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+    tags: ["Drupal", "PHP", "MySQL", "HTML", "CSS", "JavaScript"],
+    category: "web app",
+    webapp: "https://www.rj.gov.br/saude/",
+  },
+  {
+    id: 3,
+    title: "Cidade Integrada",
+    description:"Web site de um projeto específico solicitado pelo Governador do Estado do Rio de Janeiro.",
+    image:"https://camo.githubusercontent.com/3ad28aa710d18525f1fc87de056ed53c706d09979589bfd5a773df36653bad38/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+    tags: ["Drupal", "PHP", "MySQL", "HTML", "CSS", "JavaScript"],
+    category: "web app",
+    webapp: "https://www.rj.gov.br/cidadeintegrada/",
+  },
+  {
+    id: 10,
+    title: "Intranet PRODERJ",
+    description:"Intranet utilizada pelos funcionários do PRODERJ",
+    image:"https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
+    tags: ["Drupal", "PHP", "MySQL", "HTML", "CSS", "JavaScript"],
+    category: "web app",
+  },
+  {
+    id: 4,
+    title: "Calculadora de IMC",
+    description:"Calculadora de IMC",
+    image:"https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
+    tags: ["HTML", "CSS", "JavaScript"],
+    category: "machine learning",
+    github: "https://github.com/Gian-lucca/Calculadora-de-IMC",
+  },
+  {
+    id: 5,
+    title: "Sistema Financeiro",
+    description:"Sistema Financeiro.",
+    image:"https://camo.githubusercontent.com/84ac6ab6f378348ef28d8184062b7e9e3511a1252ae3966eaa49e8e998f732a7/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+    tags: ["Typescript", "HTML", "CSS"],
+    category: "machine learning",
+    github: "https://github.com/Gian-lucca/expense-tracker",
+  },
+];
+
